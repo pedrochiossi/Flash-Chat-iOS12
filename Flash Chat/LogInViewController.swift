c7fa360dadf1aea8a2ced00fbc1acf936ff9adcb
Pedro Chiossi <pchiossi@gmail.com>
2019-06-28 19:14:11 -0300

    Add retrieve messages method from firebase API, finish the view.

--- Flash Chat/LogInViewController.swift
+++ Flash Chat/LogInViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import SVProgressHUD
 
 class LogInViewController: UIViewController {
 
@@ -25,12 +26,15 @@ class LogInViewController: UIViewController {
 
    
     @IBAction func logInPressed(_ sender: AnyObject) {
-
+        
+        SVProgressHUD.show()
+        
         //TODO: Log in the user
         Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
             if error != nil {
                 print(error!)
             } else {
+                SVProgressHUD.dismiss()
                 print("Login was sucessfull")
                 self.performSegue(withIdentifier: "goToChat", sender: self)
             }
